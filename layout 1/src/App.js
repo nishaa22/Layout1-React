8cf002a1955cf300af635a34fb2f32c0f7d5228c
nishaa22 <nisha.rani@excellencetechnologies.in>
2022-06-11 16:12:54 +0530

    layout1 using bem css and sass

--- layout 1/src/App.js
+++ layout 1/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./style.scss"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+       <div class="page">
+        <nav>
+            <a class="nav__item" href="#">Layout 1</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </nav>
+        <div class="content">
+            <span class="content__heading1">Page Heading</span>&nbsp;<span class="content__heading2">Secondary content Text</span>
+            <hr/>
+            <div class="content__item">
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+                <div class="content__item__box">
+                    <img src="https://place-hold.it/700x400"/>
+                    <div class="content__item__box__text">
+                        <span>Project Name</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptates provident quis?</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+        <footer>
+            
+            <p>Copyright &copy; Your Website 2014</p>
+        </footer>
+        </div>
+        
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
